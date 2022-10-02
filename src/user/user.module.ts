3f1d80727544f07f1f070ef668ff72413fd56961
hito-kotaro <tohi.work@gmail.com>
2022-10-03 08:32:03 +0900

    entity impoerts追加

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Roles } from 'src/entity/role.entity';
+import { Teams } from 'src/entity/teams.entity';
+import { Tenants } from 'src/entity/tenant.entity';
 import { Users } from 'src/entity/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users, Tenants, Roles, Teams])],
   controllers: [UserController],
   providers: [UserService],
 })
