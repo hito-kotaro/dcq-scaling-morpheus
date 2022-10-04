4cc9b63d6b3c9b1346b2fbbb2f3dd7efbd0fe258
hito-kotaro <tohi.work@gmail.com>
2022-10-04 23:39:53 +0900

    テストコード準備

--- src/v1/tenant/tenant.controller.spec.ts
+++ src/v1/tenant/tenant.controller.spec.ts
@@ -1,14 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { request } from 'express';
+import { Tenants } from 'src/entity/tenant.entity';
+import { Repository } from 'typeorm';
 import { TenantController } from './tenant.controller';
+import { TenantService } from './tenant.service';
 
 describe('TenantController', () => {
   let controller: TenantController;
+  let mockRepository: Repository<Tenants>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TenantController],
+      providers: [
+        TenantService,
+        { provide: getRepositoryToken(Tenants), useClass: Repository },
+      ],
     }).compile();
 
+    mockRepository = module.get<Repository<Tenants>>(
+      getRepositoryToken(Tenants),
+    );
     controller = module.get<TenantController>(TenantController);
   });
 
--- src/v1/tenant/tenant.service.spec.ts
+++ src/v1/tenant/tenant.service.spec.ts
@@ -1,14 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Tenants } from 'src/entity/tenant.entity';
+import { Repository } from 'typeorm';
+import { TenantController } from './tenant.controller';
 import { TenantService } from './tenant.service';
 
 describe('TenantService', () => {
   let service: TenantService;
-
+  let mockRepository: Repository<Tenants>;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TenantService],
+      controllers: [TenantController],
+      providers: [
+        TenantService,
+        { provide: getRepositoryToken(Tenants), useClass: Repository },
+      ],
     }).compile();
 
+    mockRepository = module.get<Repository<Tenants>>(
+      getRepositoryToken(Tenants),
+    );
     service = module.get<TenantService>(TenantService);
   });
 
